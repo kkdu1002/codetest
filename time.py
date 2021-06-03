95651e15d9241047a7f009a9aa912609a1ff594e
kkdu1002 <63584294+kkdu1002@users.noreply.github.com>
2021-06-03 16:34:43 +0900

    Add files via upload

---
+++ time.py
@@ -0,0 +1,10 @@
+# N시59분59초까지의 시간중에서 3이 하나라도 포함되는 모든 경우의 수
+n = int(input())
+count = 0
+for h in range(n+1):
+    for m in range(60):
+        for s in range(60):
+            if "3" in str(h) + str(m) + str(s):
+                count += 1
+print(count)
+print(h,"+",m,"+",s)
